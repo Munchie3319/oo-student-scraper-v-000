71bd1b9edff7602d7c082f5bacffaa20300738a8
rosa lee <rosalee0116@gmail.com>
2020-05-13 22:03:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,21 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = [ ]
+   doc = Nokogiri::HTML(open(index_url))
+   doc.css(".student-card").each do |student|
+     student_info = { }
+     student_info[:name] = student.css("h4").text
+     student_info[:location] = student.css("p").text
+     #binding.pry
+     student_info[:profile_url] = student.css("a").attr("href").value
+     students << student_info
+   end
+   students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
